744903769840d0457f11fa1962b9e4f586ab538b
Jonathan Arbey Rivera <arbeyjr@gmail.com>
2024-07-30 19:26:08 -0500

    crear componentes iniciales y configuracion sidenav con rutas

--- src/app/UI/main/app.component.ts
+++ src/app/UI/main/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+    LayoutModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-ngrx';
+
+  @ViewChild(MatSidenav, { static: true }) sidenav!: MatSidenav;
+
+  constructor(
+    private observer: BreakpointObserver,
+  ) { }
+
+  ngOnInit(): void {
+    this.observer.observe(['(max-width: 800px)']).subscribe((result) => {
+      if (result.matches) {
+        this.sidenav.mode = 'over'; // ocultar
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side'; // mostrar
+        this.sidenav.open();
+      }
+    });
+  }
 }
--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../UI/pages/home/home.component';
+import { PostComponent } from '../UI/pages/post/post.component';
+import { PhotosComponent } from '../UI/pages/photos/photos.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
+
+  { path: '', component: HomeComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 
 ];
